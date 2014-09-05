d3eee661f2d26a5c02191534185cc40dbe820e17
alf-raul <alf-raul@localhost>
2014-09-05 07:05:06 -0530

    Add class model and migration script

---
+++ migration/oracle/822AlterTable_HRDay_oracle.sql
@@ -0,0 +1,5 @@
+-- Sep 4, 2014 9:18:25 AM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=16,Updated=TO_DATE('2014-09-04 09:18:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000009
+;
+
---
+++ migration/oracle/826AddCreateDay_oracle.sql
@@ -0,0 +1,30 @@
+-- 4/09/2014 01:52:28 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Create Day',Updated=TO_DATE('2014-09-04 13:52:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=1000000
+;
+
+-- 4/09/2014 01:52:28 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=1000000
+;
+
+-- 4/09/2014 01:52:28 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Create Day',Updated=TO_DATE('2014-09-04 13:52:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=1000000
+;
+
+-- 4/09/2014 01:52:28 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=1000000
+;
+
+-- 4/09/2014 01:52:29 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Create Day',Updated=TO_DATE('2014-09-04 13:52:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=1000007
+;
+
+-- 4/09/2014 01:52:29 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=1000007
+;
+
